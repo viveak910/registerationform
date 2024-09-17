4c90ce4f76349fe422630e3833350dcd48f3fb1d
viveak910 <1602-23-737-191@vce.ac.in>
2024-09-17 17:26:49 +0530

    issuses resloved

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 import RegistrationFormComponent from "@/components/registration-form";
 export default function Home() {
   return (
--- components/registration-form.tsx
+++ components/registration-form.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { toast } from "@/hooks/use-toast"
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card"
+import { Card, CardHeader, CardTitle, CardDescription, CardContent} from "@/components/ui/card"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 
 export default function RegistrationFormComponent() {
